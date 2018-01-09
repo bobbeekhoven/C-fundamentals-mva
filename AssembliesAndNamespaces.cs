69052f7edd7be5b9df06d851d45dbc7e985acf24
bobbeekhoven <bob.vanbeekhoven@itvitaelearning.nl>
2018-01-09 13:36:19 +0100

    Add files via upload

---
+++ AssembliesAndNamespaces.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace AssembliesAndNamespaces
+{
+    class AssembliesAndNamespaces
+    {
+        static void Main(string[] args)
+        {
+           
+            //string text = "We want to write this to our file. ";
+             //System.IO.File.WriteAllText(@"C:\Lesson17\WriteText.txt", text);
+
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
+
+
+            Console.WriteLine(reply);
+            File.WriteAllText(@"C:\Lesson17\WriteText.txt", reply);
+            Console.ReadLine();
+
+        }
+    }
+}
