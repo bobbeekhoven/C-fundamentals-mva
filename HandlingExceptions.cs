0ca43266f2056e08e5f106b86303d5b6196c60e7
bobbeekhoven <bob.vanbeekhoven@itvitaelearning.nl>
2018-01-16 14:01:52 +0100

    Add files via upload

---
+++ HandlingExceptions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+
+namespace HandlingExceptions
+{
+    class HandlingExceptions
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"C:\Lesson22\Example.txt");
+                Console.WriteLine(content); 
+            }
+            catch (FileNotFoundException  ex)
+            {
+                Console.WriteLine("There was a problem! (Kenny changed his name!)");
+                Console.WriteLine("Make sure the name of the file is named correctly: Example.txt");
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("There was a Problem! (Kenny does not exist!)");
+                Console.WriteLine(@"Make sure the directory C:\Lesson 22 exists");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("There was a problem! (Kenny's missing!)");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // Code top finalize
+                // Setting objects to null
+                // Closing database connections
+                Console.WriteLine("Closing application now ... (Erasing Kenny!)");
+            }
+            Console.ReadLine();
+        }
+    }
+}
